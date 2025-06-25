ce574c654e6afa25aad0358a410d7f414bd39588
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-06-25 20:43:12 +0530

    basic backend setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("HI")
+import { WebSocketServer } from "ws"
+import http from "http"
+import { MessageHandler } from "./managers/MessageHandler"
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200)
+    res.end("WS server is rrunning")
+
+})
+
+const wss = new WebSocketServer({ server })
+const messageHandler = new MessageHandler()
+
+wss.on("connection", (ws) => {
+    messageHandler.handleConnection(ws)
+})
+
+const PORT = 8080
+server.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+
