7288be2be823c8300a3fff31fd851627a8476a89
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-07-02 21:04:41 +0530

    update connection manager to support multiple rooms

--- server/src/managers/ConnectionManager.ts
+++ server/src/managers/ConnectionManager.ts
@@ -1,33 +1,44 @@
-import WebSocket from "ws"
-import { Message } from "../types"
+import WebSocket from "ws";
+import { Message, RoomId } from "../types";
 export class ConnectionManager {
-    private static instance: ConnectionManager
-    private clients = new Set<WebSocket>
+  private static instance: ConnectionManager;
+  private rooms = new Map<RoomId, Set<WebSocket>>();
 
-    private constructor() { }
+  private constructor() {}
 
-    static getInstance() {
-        if (!ConnectionManager.instance) {
-            ConnectionManager.instance = new ConnectionManager()
+  static getInstance() {
+    if (!ConnectionManager.instance) {
+      ConnectionManager.instance = new ConnectionManager();
+    }
+    return ConnectionManager.instance;
+  }
 
-        }
-        return ConnectionManager.instance
+  addClient(roomId: RoomId, ws: WebSocket) {
+    if (!this.rooms.has(roomId)) {
+      this.rooms.set(roomId, new Set());
     }
+    this.rooms.get(roomId)?.add(ws);
+    console.log("ADD", roomId);
+  }
+  removeClient(roomId: RoomId, ws: WebSocket) {
+    const clients = this.rooms.get(roomId);
+    if (clients) {
+      clients.delete(ws);
+      if (clients.size === 0) {
+        this.rooms.delete(roomId);
+      }
+    }
+    console.log("REMOVE", roomId);
+  }
+  broadcast(message: Message, except?: WebSocket) {
+    const clients = this.rooms.get(message.roomId);
+    if (!clients) return;
 
-    addClient(ws: WebSocket) {
-        this.clients.add(ws)
-        console.log("Client Added ", this.clients.size)
-    }
-    removeClient(ws: WebSocket) {
-        this.clients.delete(ws)
-    }
-    broadcast(message: Message, except?: WebSocket) {
-        const json = JSON.stringify(message)
-        this.clients.forEach(client => {
-            console.log("Sent to ", client)
-            if (client !== except && client.readyState === WebSocket.OPEN) {
-                client.send(json)
-            }
-        })
-    }
+    const json = JSON.stringify(message);
+    clients.forEach((client) => {
+      if (client !== except && client.readyState === WebSocket.OPEN) {
+        client.send(json);
+      }
+    });
+  }
 }
