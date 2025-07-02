d6b7be7f6439ca618a512f7a9862de14c5c38439
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-07-02 21:04:22 +0530

    types to support multiple rooms

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,11 +1,22 @@
-export type Message = DrawMessage
+import { WebSocket } from "ws";
+export type RoomId = string;
+
+export type Message = JoinMessage | DrawMessage;
 
 export type DrawMessage = {
-    type: "draw",
-    payload: {
-        startX: number,
-        startY: number,
-        endX: number,
-        endY: number
-    }
-}
+  type: "draw";
+  roomId: RoomId;
+  payload: {
+    startX: number;
+    startY: number;
+    endX: number;
+    endY: number;
+  };
+};
+
+export type JoinMessage = {
+  type: "join";
+  roomId: RoomId;
+};
+
+export type WSWithRoom = WebSocket & { roomId?: RoomId };
