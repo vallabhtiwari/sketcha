780b1cab2d39ab6c03ce735130d3b8681b0ea5ba
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-07-05 00:18:48 +0530

    express to handle http requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
+import express from "express";
 import { WebSocketServer } from "ws";
 import http from "http";
 import { MessageHandler } from "./managers/MessageHandler";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("WS server is rrunning");
-});
+const PORT = 8080;
+const app = express();
+const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 const messageHandler = new MessageHandler();
@@ -14,5 +14,8 @@ wss.on("connection", (ws) => {
   messageHandler.handleConnection(ws);
 });
 
-const PORT = 8080;
+app.get("/", (req, res) => {
+  res.json("HTTP service running...");
+});
+
 server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
