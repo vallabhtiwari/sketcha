0beb28d6688b79ee69add604a86de2b3c52aa615
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-07-05 23:53:58 +0530

    tailwind for styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,9 @@ function App() {
   const [roomId, setRoomId] = useState<RoomId>("");
   const [ws, setWs] = useState<WebSocket | null>(null);
   const handleJoin = (roomId: RoomId) => {
-    const socket = new WebSocket("ws://localhost:8080");
+    const socket = new WebSocket(
+      "ws://localhost:8080?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbWNwN255a2UwMDAwcHdvN2ZsNHdvZXEzIiwiZW1haWwiOiJ0ZXN0MUB0ZXN0LmNvbSIsIm5hbWUiOiJ0ZXN0MSIsImlhdCI6MTc1MTY5OTkxM30.lDmrxyNwdOvlC34kND3zqUU8Pgh6GygbFcdRQqfeuVs"
+    );
     socket.onopen = () =>
       socket.send(
         JSON.stringify({
