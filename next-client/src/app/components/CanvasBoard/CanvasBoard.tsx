7cc938dfd5fa437cf43ab7bd9a73bf0543285f23
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-07-09 01:27:05 +0530

    color and width selector for brush

--- next-client/src/app/components/CanvasBoard/CanvasBoard.tsx
+++ next-client/src/app/components/CanvasBoard/CanvasBoard.tsx
@@ -1,24 +1,26 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import * as fabric from "fabric";
 import { Canvas } from "./_Canvas";
-import type { CanvasBoardProps, DrawMessage } from "../../types";
+import type { CanvasBoardProps, DrawMessage } from "@/types";
 
 export const CanvasBoard = ({ ws, roomId }: CanvasBoardProps) => {
   const ref = useRef<fabric.Canvas>(null);
+  const [showMenu, setShowMenu] = useState(false);
+  const [brushColor, setBrushColor] = useState("#000000");
+  const [brushWidth, setBrushWidth] = useState(3);
 
   const onLoad = useCallback(
     (canvas: fabric.Canvas) => {
-      canvas.setDimensions({
-        width: window.innerWidth - 20,
-        height: window.innerHeight - 20,
-      });
+      const resizeCanvas = () => {
+        const el = canvas.lowerCanvasEl;
+        if (el && el.parentElement?.parentElement) {
+          const { width, height } =
+            el.parentElement.parentElement.getBoundingClientRect();
+          canvas.setDimensions({ width, height });
+        }
+      };
       canvas.isDrawingMode = true;
       canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
-      const brush = canvas.freeDrawingBrush;
-      if (brush) {
-        brush.width = 3;
-        brush.color = "#000000";
-      }
       canvas.on("path:created", (e) => {
         const path = e.path as fabric.Path;
         if (!path) return;
@@ -47,13 +49,71 @@ export const CanvasBoard = ({ ws, roomId }: CanvasBoardProps) => {
           console.error("Failed to load remote drawing:", err);
         }
       };
+      resizeCanvas();
+      window.addEventListener("resize", resizeCanvas);
+      return () => window.removeEventListener("resize", resizeCanvas);
     },
-    [ref]
+    [ws, roomId]
   );
 
+  useEffect(() => {
+    const canvas = ref.current;
+    if (canvas && canvas.freeDrawingBrush) {
+      canvas.freeDrawingBrush.color = brushColor;
+      canvas.freeDrawingBrush.width = brushWidth;
+    }
+  }, [brushColor, brushWidth]);
+
   return (
-    <div className="position-relative">
-      <Canvas ref={ref} onLoad={onLoad} />
+    <div className="relative flex-1 flex flex-col bg-white dark:bg-gray-900 overflow-hidden">
+      <div className="absolute m-2 z-1 flex justify-start">
+        <button
+          onClick={() => setShowMenu(!showMenu)}
+          className="text-xl px-3 py-2 font-bold hover:cursor-pointer bg-muted rounded-md border border-primary"
+        >
+          ☰
+        </button>
+      </div>
+      {showMenu && (
+        <div className="absolute top-15 left-2 bg-white dark:bg-gray-800 p-4 shadow-md z-50 rounded-md border border-primary">
+          <div className="space-y-2">
+            <div className="flex items-center">
+              <label className="text-sm font-medium text-gray-700 dark:text-gray-300">
+                Color
+              </label>
+              <input
+                type="color"
+                value={brushColor}
+                onChange={(e) => setBrushColor(e.target.value)}
+                className="ml-1 w-8 h-6 p-0 border-none outline-none"
+                // style={{ background: "white" }}
+              />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <label className="text-sm font-medium text-gray-700 dark:text-gray-300">
+                Width
+              </label>
+              <input
+                type="range"
+                min={1}
+                max={20}
+                value={brushWidth}
+                onChange={(e) => setBrushWidth(parseInt(e.target.value))}
+                className="w-full accent-primary m-0 p-0"
+              />
+              <span>{brushWidth}</span>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div
+        className="flex-1"
+        onPointerDown={() => setShowMenu && setShowMenu(false)}
+      >
+        <Canvas ref={ref} onLoad={onLoad} />
+      </div>
     </div>
   );
 };
--- next-client/src/app/page.tsx
+++ next-client/src/app/page.tsx
@@ -1,26 +1,7 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
-import { RoomId } from "@/types";
 import { Main } from "@/app/components/Main";
 
 export default function Home() {
-  const [roomId, setRoomId] = useState<RoomId>("");
-  const [ws, setWs] = useState<WebSocket | null>(null);
-  const handleJoin = (roomId: RoomId) => {
-    const socket = new WebSocket(
-      "ws://localhost:8080?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbWNwN255a2UwMDAwcHdvN2ZsNHdvZXEzIiwiZW1haWwiOiJ0ZXN0MUB0ZXN0LmNvbSIsIm5hbWUiOiJ0ZXN0MSIsImlhdCI6MTc1MTY5OTkxM30.lDmrxyNwdOvlC34kND3zqUU8Pgh6GygbFcdRQqfeuVs"
-    );
-    socket.onopen = () =>
-      socket.send(
-        JSON.stringify({
-          type: "join",
-          roomId,
-        })
-      );
-    setWs(socket);
-    setRoomId(roomId);
-  };
-
   return <Main />;
 }
