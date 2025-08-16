92a683bc4fa231d0ebbcf4e121b9d6a6a7b2901e
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-08-16 17:46:26 +0530

    implement refresh route

--- server/src/routers/authRouter.ts
+++ server/src/routers/authRouter.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 import { userSigninSchema, userSignupSchema } from "../zodSchemas";
 import { prisma } from "../db";
 
 const router = express.Router();
-
+const isDev = process.env.NODE_ENV === "development";
 router.post("/signup", async (req, res) => {
   const parsedBody = userSignupSchema.safeParse(req.body);
   if (!parsedBody.success) {
@@ -74,13 +74,62 @@ router.post("/signin", async (req, res) => {
       email: userFound.email,
       name: userFound.name,
     };
-    const token = jwt.sign(payload, process.env.JWT_SECRET || "SUPER_SECRET", {
-      expiresIn: "5m",
+    const accessToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET || "SUPER_SECRET",
+      {
+        expiresIn: "5m",
+      }
+    );
+    const refreshToken = jwt.sign(
+      payload,
+      process.env.JWT_SECRET || "SUPER_SECRET",
+      {
+        expiresIn: "1d",
+      }
+    );
+    console.log("refreshToken", isDev, !isDev);
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: !isDev,
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: isDev ? "lax" : "none",
     });
-    res.json({ token });
+    res.json({ accessToken });
   } catch (err) {
     console.error("Signin error:", err);
     res.status(401).json({ error: "Invalid credentials" });
+  }
+});
+
+router.post("/refresh", async (req, res) => {
+  try {
+    console.log("Refresh token:", req.cookies);
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+    const payload = jwt.verify(
+      refreshToken,
+      process.env.JWT_SECRET || "SUPER_SECRET"
+    ) as JwtPayload;
+    const userPayload = {
+      userId: payload.userId,
+      email: payload.email,
+      name: payload.name,
+    };
+    const accessToken = jwt.sign(
+      userPayload,
+      process.env.JWT_SECRET || "SUPER_SECRET",
+      {
+        expiresIn: "5m",
+      }
+    );
+    res.json({ accessToken });
+  } catch (err) {
+    console.error("Refresh error:", err);
+    res.status(401).json({ error: "Unauthorized" });
   }
 });
 
