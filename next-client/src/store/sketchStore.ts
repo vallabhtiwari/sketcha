223a6ddf9e6ac53df336eb9fe574b7ef39f522a0
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-08-22 03:29:33 +0530

    store font size,weght and family

--- next-client/src/store/sketchStore.ts
+++ next-client/src/store/sketchStore.ts
@@ -4,10 +4,27 @@ import { CanvasToolOptions } from "@/types";
 export const canvasBackgroundOptions = [
   "#FFFBF8",
   "#FFF0E6",
-  "#FFB366",
   "#E8864A",
   "#2A1D15",
+  "#261410",
 ];
+
+export const fontSizeOptions = {
+  Small: 16,
+  Medium: 24,
+  Large: 32,
+};
+
+export const fontWeightOptions = {
+  Normal: 500,
+  Bold: 700,
+};
+
+export const fontFamilyOptions = {
+  Sans: "sans-serif",
+  Serif: "serif",
+  Mono: "monospace",
+};
 
 interface SketchState {
   showMenu: boolean;
@@ -16,12 +33,18 @@ interface SketchState {
   canvasBackground: string;
   selectedTool: CanvasToolOptions;
   isToolLocked: boolean;
+  fontSize: number;
+  fontWeight: number;
+  fontFamily: string;
   setBrushColor: (color: string) => void;
   setBrushWidth: (width: number) => void;
   setCanvasBackground: (color: string) => void;
   setSelectedTool: (tool: CanvasToolOptions) => void;
   setShowMenu: (show: boolean) => void;
   setIsToolLocked: (locked: boolean) => void;
+  setFontSize: (size: number) => void;
+  setFontWeight: (weight: number) => void;
+  setFontFamily: (family: string) => void;
 }
 
 export const useSketchStore = create<SketchState>((set) => ({
@@ -31,10 +54,16 @@ export const useSketchStore = create<SketchState>((set) => ({
   canvasBackground: canvasBackgroundOptions[0],
   selectedTool: "pencil",
   isToolLocked: false,
+  fontSize: fontSizeOptions.Medium,
+  fontWeight: fontWeightOptions.Normal,
+  fontFamily: fontFamilyOptions.Sans,
   setBrushColor: (color) => set({ brushColor: color }),
   setBrushWidth: (width) => set({ brushWidth: width }),
   setCanvasBackground: (color) => set({ canvasBackground: color }),
   setSelectedTool: (tool) => set({ selectedTool: tool }),
   setShowMenu: (show) => set({ showMenu: show }),
   setIsToolLocked: (locked) => set({ isToolLocked: locked }),
+  setFontSize: (size) => set({ fontSize: size }),
+  setFontWeight: (weight) => set({ fontWeight: weight }),
+  setFontFamily: (family) => set({ fontFamily: family }),
 }));
